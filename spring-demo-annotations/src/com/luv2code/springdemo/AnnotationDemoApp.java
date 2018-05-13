7f18fceb31ff4f0fcde5d0920e1053f42e8a32d6
Felipe D'Alessio <felipe.dalessio123@gmail.com>
2018-05-13 16:00:06 -0300

    Atualização de codigo

--- spring-demo-annotations/src/com/luv2code/springdemo/AnnotationDemoApp.java
+++ spring-demo-annotations/src/com/luv2code/springdemo/AnnotationDemoApp.java
@@ -6,13 +6,13 @@ public class AnnotationDemoApp {
 
 	public static void main(String[] args) {
 
-		ClassPathXmlApplicationContext context = 
-				new ClassPathXmlApplicationContext("applicationContext.xml");
-		
-		Coach theCoach = context.getBean("thatSillyCoach", Coach.class);
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+		Coach theCoach = context.getBean("tennisCoach", Coach.class);
 
 		System.out.println(theCoach.getDailyWorkout());
-		
+		System.out.println(theCoach.getDailyFortune());
+
 		context.close();
 	}
 
--- spring-demo-annotations/src/com/luv2code/springdemo/TennisCoach.java
+++ spring-demo-annotations/src/com/luv2code/springdemo/TennisCoach.java
@@ -1,13 +1,46 @@
 package com.luv2code.springdemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-@Component("thatSillyCoach")
+@Component
 public class TennisCoach implements Coach {
 
+	private FortuneService fortuneService;
+
+	// Injecao de dependecia pelo Construtor
+	@Autowired
+	public TennisCoach(FortuneService fortuneService) {
+		System.out.println("Inside contructor TennisCoach");
+		this.fortuneService = fortuneService;
+	}
+
+	// Injecao de dependecia pelo metodo Set
+	/*
+	@Autowired
+	public void setFortuneService(FortuneService fortuneService) {
+		System.out.println("Inside setFortuneService");
+		this.fortuneService = fortuneService;
+	}
+	*/
+	
+	// Injecao de dependecia por qualquer nmetodo
+	/*
+	@Autowired
+	public void doSomeCrazyStuff(FortuneService fortuneService) {
+		System.out.println("Inside doSomeCrazyStuff");
+		this.fortuneService = fortuneService;
+	}
+	*/
+	
 	@Override
 	public String getDailyWorkout() {
 		return "Practice your backhand volley";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		return fortuneService.getFortune();
 	}
 
 }
